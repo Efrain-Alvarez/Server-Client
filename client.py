c8aa0aa35a535bcbb76396b0f68c0714a373d829
Efrain Alvarez <61568385+Efrain-Alvarez@users.noreply.github.com>
2024-01-09 22:06:29 -0800

    Update client.py
    adding new functionality

--- client.py
+++ client.py
@@ -1,17 +1,28 @@
 import socket
+import os
+import struct
 
-def send_txt_file(file_path, server_address):
+def send_file(file_path, server_address):
     # Create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
-        # Read the file contents
+        # Get the file name from the path, does not include the path 
+        file_name = os.path.basename(file_path)
+
+        # Send filename length, ! means the order of the bytes and the I is unsigned int
+        filename_length = len(file_name)
+        client_socket.sendto(struct.pack('!I', filename_length), server_address)
+
+        # Send the filename
+        client_socket.sendto(file_name.encode(), server_address)
+
+        # Read the file contents as binary
         with open(file_path, 'rb') as file:
             file_contents = file.read()
 
-        # Send file data to the server
-        file_data = f"{file_path}|{file_contents.decode()}"
-        client_socket.sendto(file_data.encode(), server_address)
+        # Send file contents to the server
+        client_socket.sendto(file_contents, server_address)
 
         # Receive the response from the server
         response, _ = client_socket.recvfrom(1024)
@@ -22,7 +33,7 @@ def send_txt_file(file_path, server_address):
         client_socket.close()
 
 if __name__ == "__main__":
-    file_path = "test.txt"  # file name with in the directory
+    file_path = "test.txt"  # file name within the directory
     server_address = ('192.168.4.25', 12345)  # Change this to the server's address and port
 
-    send_txt_file(file_path, server_address)
+    send_file(file_path, server_address)
